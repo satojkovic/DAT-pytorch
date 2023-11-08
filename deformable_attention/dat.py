3f1f227cd31e7a6c1af6cab24532c6e0ff3ecfd2
satojkovic <satojkovic@gmail.com>
2023-11-09 00:08:31 +0900

    Update DAT class

--- deformable_attention/dat.py
+++ deformable_attention/dat.py
@@ -16,11 +16,22 @@ class LayerNormProxy(nn.Module):
 
 class DAT(nn.Module):
     def __init__(
-        self, dim_heads, num_heads, num_offset_groups, offset_kernel_size, offset_stride
+        self,
+        image_size,
+        in_channels,
+        patch_size,
+        embed_dim,
+        num_heads,
+        num_offset_groups,
+        offset_kernel_size,
+        offset_stride,
     ):
         super().__init__()
 
-        self.dim_heads = dim_heads
+        self.image_size = image_size
+        self.in_channels = in_channels
+        self.patch_size = patch_size
+
         self.num_heads = num_heads
         self.num_offset_groups = num_offset_groups
         self.offset_kernel_size = offset_kernel_size
@@ -28,10 +39,16 @@ class DAT(nn.Module):
         assert self.offset_kernel_size == self.offset_stride
         self.pad_size = 0
 
-        embed_dim = self.num_heads * self.dim_heads
+        self.embed_dim = embed_dim
+        self.dim_heads = self.embed_dim // self.num_heads
         self.offset_dims = embed_dim // self.num_offset_groups
+        assert (
+            self.offset_dims % self.num_offset_groups == 0
+        ), f"offset_dims: {self.offset_dims}, num_offset_groups: {num_offset_groups}"
 
-        self.Wq = nn.Conv2d(embed_dim, embed_dim, kernel_size=1, stride=1, padding=0)
+        self.Wq = nn.Conv2d(
+            self.embed_dim, self.embed_dim, kernel_size=1, stride=1, padding=0
+        )
 
         self.offset_net = nn.Sequential(
             nn.Conv2d(
